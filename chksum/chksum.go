e36068a66d03c6c8283bc1ed89ba946aa0f93183
Florian Hübner <code@hub.lol>
2023-02-12 00:58:54 +0100

    helper func file checksum

--- chksum/chksum.go
+++ chksum/chksum.go
@@ -31,6 +31,35 @@ func Create(reader io.Reader, algo hash.Hash, enco encoding.Scheme) (string, err
 		return "", hashutils.ErrUnknownEncoding
 	}
 }
+func FromFile(path string, algo hash.Hash, enco encoding.Scheme) (string, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return "", err
+		}
+		log.Fatalln(err.Error())
+	}
+	if info.IsDir() {
+		return "", hashutils.ErrFileIsDir
+	}
+	if !info.Mode().IsRegular() {
+		return "", hashutils.ErrFileNotRegular
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(file)
+
+	return Create(file, algo, enco)
+}
 
 func Verify(reader io.Reader, chksum string, algo hash.Hash, enco encoding.Scheme) (bool, error) {
 	c, err := Create(reader, algo, enco)
